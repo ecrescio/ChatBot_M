926402721fa832a224e06f6523d9e9e0178cf513
ecrescio <67245697+ecrescio@users.noreply.github.com>
2025-04-09 14:18:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def load_db(embeddings, path):
     return vectorstore
 
 if not os.path.exists('faiss_index'):
-    vectorstore=load_db(embeddings,'RANLP077.pdf')
+    vectorstore=load_db(embeddings,'2102.09427v2.pdf')
     vectorstore.save_local("faiss_index")
 else:
     vectorstore = FAISS.load_local("faiss_index",embeddings=embeddings,allow_dangerous_deserialization=True)
